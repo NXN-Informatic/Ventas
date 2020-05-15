033c6032033c111f0f7a7a6298f8fb0d44db7544
Nain Acero <a_nacerom@unjbg.edu.pe>
2020-05-15 00:49:47 -0500

    Prueba Shared Facebook

--- resources/views/publicas/puestos.blade.php
+++ resources/views/publicas/puestos.blade.php
@@ -1,25 +1,32 @@
-<html lang="es"><head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="lBCzchWcZqnom0lbcergpRXhAboZtQ6gPdWYYzki">
-
-    <title>{{ $puesto->name }}</title>
-
-    <meta name="description" content="Enpieza a Craer tu Puesto en Feria Tacna 2020">
-    <meta name="keywords" content="La única feria tacna en Tacna">
-    <link rel="canonical" href="http://feriatacna.com/">
-    
-    <!-- OpenGraph metadata-->
-    <meta property="og:locale" content="es">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="{{ $puesto->name }}">
-    <meta property="og:description" content="FERIA TACNA">
-    <meta property="og:url" content="http://feriatacna.com/">
-    <meta property="og:site_name" content="FERIA TACNA">
-    <meta property="og:image" content="http://feriatacna.com/storage/{{ $puesto->id }}/banner/{{ $puesto->banner }}">
+<html>
+<head>
+  <title>{{ $puesto->name }}</title>
+    <!-- You can use Open Graph tags to customize link previews.
+    Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
+  <meta property="og:url"           content="http://feriatacna.com/" />
+  <meta property="og:type"          content="website" />
+  <meta property="og:title"         content="{{ $puesto->name }}" />
+  <meta property="og:description"   content="FERIA TACNA" />
+  <meta property="og:image"         content="http://feriatacna.com/storage/{{ $puesto->id }}/banner/{{ $puesto->banner }}" />
+</head>
 <body>
+
+  <!-- Load Facebook SDK for JavaScript -->
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  <!-- Your share button code -->
+  <div class="fb-share-button" 
+    data-href="https://www.your-domain.com/your-page.html" 
+    data-layout="button_count">
+  </div>
+
 </body>
 </html>
 
