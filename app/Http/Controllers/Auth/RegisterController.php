e5f853d61b5aed02235fc96fd6519f94df7f3a86
Nain Acero <a_nacerom@unjbg.edu.pe>
2020-05-20 20:33:59 -0500

    parche bug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -74,7 +74,6 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-        dd($user);
         UsuarioPuesto::create([
             'usuario_id' => $user->id,
             'puesto_id'  => $puesto->id
