72d6897f4a7e0fc6ddbfe2239882e8f9b3d4fa43
x4vYOR <x4vyjm@gmail.com>
2020-05-27 18:47:38 -0500

    update columns to product table

---
+++ database/migrations/2020_05_27_233850_update_columns_to_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateColumnsToProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('productos', function (Blueprint $table) {
+            $table->string('disponible')->default('in stock')->nullable();
+            $table->string('condicion',20)->default('new')->nullable();
+            $table->string('marca',30)->default('Propia')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('productos', function (Blueprint $table) {
+            $table->dropColumn('disponible');
+            $table->dropColumn('condicion');
+            $table->dropColumn('marca');
+        });
+    }
+}
