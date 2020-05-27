cce0ac12c9fc04274435196a2661c567736a7ceb
Nain Acero <a_nacerom@unjbg.edu.pe>
2020-05-27 00:11:04 -0500

    Letra :v

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -10,10 +10,10 @@
 
     <title>@yield('title') - FeriaTacna</title>
 
-    <meta property="og:title" content="@yield('ogTitle', 'PUESTO NAIN 2020.')"/>
+    <meta property="og:title" content="@yield('ogTitle', 'FERIA TACNA.')"/>
     <meta property="og:site_name" content="FERIA TACNA"/>
     <meta property="og:url" content="@yield('ogUrl', 'http://feriatacna.com/')"/>
-    <meta property="og:description" content="@yield('ogDesc', 'La única feria Onlay en Tacna 2020 , encuentre sus productos que usted requiera.')"/>
+    <meta property="og:description" content="@yield('ogDesc', 'La única feria online en Tacna 2020 , encuentre sus productos que usted requiera.')"/>
     <meta property="og:type" content="@yield('ogType', 'website')"/>
     <meta property="og:locale" content="es"/>
     <meta property="og:image" content="@yield('ogImage', 'http://feriatacna.com/img/banner/banner.jpg')"/>
