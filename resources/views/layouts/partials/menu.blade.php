197e4c1c6ee416def710137803147b8743ef5c13
Nain Acero <a_nacerom@unjbg.edu.pe>
2020-06-04 17:41:36 -0500

    awdwa wad wad wad wa

--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -8,7 +8,11 @@
         </a>
         <div class="sidebar-content">
             <div class="sidebar-user">
-                <img src="{{ asset('img/user.png') }}" class="img-fluid rounded-circle mb-2" alt="Linda Miller" />
+                @if(auth()->user()->imagen)
+                    <img src="{{ asset('storage/usuarios/'.auth()->user()->id.'/'.auth()->user()->imagen) }}" class="img-fluid rounded-circle mb-2">
+                @else
+                    <img src="{{ asset('img/user.png') }}" width="140" height="140" class="rounded-circle mt-2">
+                @endif
                 <div class="font-weight-bold">{{ auth()->user()->name }}</div>
                 <small>{{ __('Feria Tacna') }}</small>
             </div>
