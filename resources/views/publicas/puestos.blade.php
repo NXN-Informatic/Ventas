852675d5cfa83a6bd00251fc8deee09dfbcad2e4
Nain Acero <a_nacerom@unjbg.edu.pe>
2020-06-11 20:31:27 -0500

    Mydad aw wadnwandwad

--- resources/views/publicas/puestos.blade.php
+++ resources/views/publicas/puestos.blade.php
@@ -140,9 +140,10 @@
 </div> -->
 
 
+
 @foreach($puesto->puestosubcategorias as $puestosubcategorias)
     @foreach($puestosubcategorias->grupos as $grupos)
-        <div class="featureProduct singleProduct" id="tiendas">
+        <div class="featureProduct singleProduct" id="tiendas" style="margin-top:-5px">
             <div class="feature__wrap container">
             <h4 class="title">{{ $grupos->name }}</h4>
             <div class="feature__filter">
@@ -280,7 +281,38 @@
     </div>
 </div>
 
-@include('layouts.components.footer')  
+<!--Start Footer-->
+<div class="footer" style="background:#F5F5F5;color:#000; border: 1px solid #ccc; padding: 0px;margin: 0px;" >
+    <div class="footer__wrap dflex">
+
+        <div class="footer__item col-lg-1 col-sm-6 col-12"></div>
+        <div class="footer__item col-lg-5 col-sm-6 col-12">
+            <h2 style="font-size: 20px">Contacto</h2><br>
+            <p style="color:#000;font-size: 17px">
+            feriaTacna@gmail.com</p>
+            <p style="color:#000">
+                Tacna, Perú</p>
+            
+            <li class="social" style="list-style: none;">
+                <a href="#" style="color:#000; font-size:20px"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" style="color:#000; font-size:20px; margin-left:5px"><i class="fab fa-twitter"></i></a>
+                <a href="#" style="color:#000; font-size:20px; margin-left:5px"><i class="fas fa-envelope"></i></a>
+            </li>
+        </div>
+        <div class="footer__item col-lg-1 col-sm-6 col-12">
+            
+        </div>
+        <div class="footer__item col-lg-5 col-sm-6 col-12">
+            <br>    
+            <img src="{{ asset('/img/logo.png') }}" alt="" style="display:block;width: 200px;margin:auto">
+            <br>    
+            <h2 style="text-align: center;">
+            © Comercio Local, todos los derechos reservados.
+            </h2>
+        </div>
+    </div>
+</div>
+<!--End Footer-->
 
 @endsection
 
