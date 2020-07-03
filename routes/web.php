b8b3e11604562e7d5a5f8d73a8b599734238fb39
x4vYOR <x4vyjm@gmail.com>
2020-07-02 21:09:04 -0500

    gfdseefsd

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Puesto;
 
 Route::get('/', function () {
     $puestos = collect();
-    $pst = Puesto::where('completado',1)->where('personalizado',1)->get()->random(8);
+    $pst = Puesto::where('completado',1)->where('personalizado',1)->get()->random(4);
     $productos = Producto::all()->random(8);
     $tiendas = collect();
     $categorias = Categoria::all();
