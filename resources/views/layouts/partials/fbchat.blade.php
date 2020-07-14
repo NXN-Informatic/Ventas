af230e8cbb172c9af48dcd3dc3460912fe268166
x4vYOR <x4vyjm@gmail.com>
2020-07-13 23:34:21 -0500

    fdsfsddfgfaaaa

--- resources/views/layouts/partials/fbchat.blade.php
+++ resources/views/layouts/partials/fbchat.blade.php
@@ -6,7 +6,7 @@
     theme_color="#bf0000"
     logged_in_greeting="¡Hola {{auth()->user()->name}}!, ¿Tienes una consulta? Estamos dispuestos a ayudarte."
     logged_out_greeting="¡Hola {{auth()->user()->name}}!, ¿Tienes una consulta? Estamos dispuestos a ayudarte."
-    margin-bottom:100px>
+    style="bottom:50pt">
     </div>
 @else
     <div class="fb-customerchat"
@@ -14,6 +14,7 @@
     page_id="106833667726609"
     theme_color="#bf0000"
     logged_in_greeting="¡Hola!, ¿Tienes una consulta? Estamos dispuestos a ayudarte."
-    logged_out_greeting="¡Hola!, ¿Tienes una consulta? Estamos dispuestos a ayudarte.">
+    logged_out_greeting="¡Hola!, ¿Tienes una consulta? Estamos dispuestos a ayudarte."
+    style="bottom:50pt">
     </div>
 @endif
